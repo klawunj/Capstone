d3b0e66e417bde03dd02955518d8171f8170ec34
keskikkw <kate.keskikyla@hotmail.com>
2017-02-22 16:04:00 -0500

    Update UartTest.ino

--- CapstoneTest/UARTTest/UartTest/UartTest.ino
+++ CapstoneTest/UARTTest/UartTest/UartTest.ino
@@ -1,21 +1,24 @@
 #include <SoftwareSerial.h>
 #include <stdlib.h>
 
-String test;
-int contest;
-String Done = "";
+//Variables
+String test;//variable to get the serial value
+int contest;//store the integer value of the serial value
+String Speed = "";//store the integer speed value
+String Turn = "";//store the turn value
+int intSpeed, intTurn;
+
+//Flags and checks
+int count = 0;//ensure that each character is read only once
+bool flag = 0;//ensure that full message is only printed once
+bool variable = 0;//signifies the switch between speed and turn when reading values
 
 
 void setup() {
   Serial.begin(9600);
-  
-  while(!Serial){
-    
+  while(!Serial){ 
   }
-
   Serial.println("On");
-  
-
 }
 
 void loop() {
@@ -24,37 +27,56 @@ void loop() {
   }
 
   contest = test.toInt();
-
-  contest = contest - 48;
-
-
-  if(contest == -16){
-    Done = Done + " ";
+  contest = contest - 48; //convert ascii values to usable numbers
+  
+  //If there is a space, switch which variable written to
+  if(contest == -16 && count == 0){
+    variable = 1;
+    count++;
   }
+  
+  //if there is a precent sign, new character
+  else if (contest == -11){
+    count = 0;
+  }
+  
+  //If there is #, means end of message
   else if (contest == -13){
-    Serial.println(Done);
-  }
-  else if (contest == -3){
-    Done = Done + "-";
-  }
-  else{
-    if(contest >= 0){
-      Done = Done + contest;
+    //check if the first time sent
+    if (flag==0){
+      
+      intSpeed = Speed.toInt();
+      intTurn = Turn.toInt();
+      Serial.println("Speed is " + Speed);
+      Serial.println(intSpeed);
+      Serial.println("Turn is " + Turn);
+      Serial.println(intTurn);
+      
+      //reset values
+      contest = 0;
+      Speed = "";
+      Turn = "";
+      flag = 1;
+      count = 0;
+      variable = 0;
     }
   }
-
-
   
-//  if (mySerial.available()){
-//    Serial.write(mySerial.read());
-//  }
-//
-//  delay(5000);
-//
-//  if(Serial.available()){
-//    mySerial.write(Serial.read());
-//  }
-//
-//  delay(5000);
+  //if there is a negative sign, put a negative sign
+  else if (contest == -3 && count == 0){
+    if (variable ==1){Turn = Turn+"-";}
+    else{Speed = Speed+"-";}
+    count++;
+    flag = 0;
+  }
 
+  //Add the number value to the message
+  else{
+    if(contest >= 0 && count == 0 ){
+      if (variable ==1){Turn = Turn+contest;}
+      else{Speed = Speed+contest;}
+      count++;
+      flag = 0;
+    }
+  }
 }

41e0cf6a4cb29eee7d40f70ed66ac65718fff836
klawunj <jon_klawunn@hotmail.ca>
2017-02-22 15:20:55 -0500

    Added in Code for IRSensors

--- CapstoneArduino/Main/Sensors.cpp
+++ CapstoneArduino/Main/Sensors.cpp
@@ -1,11 +1,41 @@
 /*****************************************************************************
  *                                 Includes
  ****************************************************************************/
-
+#include "Sensors.h"
  /*****************************************************************************
  *                             Global Variables
  ****************************************************************************/
+ 
 
   /*****************************************************************************
  *                                 Functions
  ****************************************************************************/
+
+ void InitSensors(){
+  pinMode(IRPin, INPUT);
+ }
+
+ int getIRValue(){
+  unsigned int reading = analogRead(IRPin);
+  return reading;
+  
+ }
+
+ SensorBehaviour getSensorBehaviour(){
+  SensorBehaviour output;
+  int SensorVal = getIRValue();
+
+  if(SensorVal >= IRStopThresh){
+    output = STOP;
+  }
+  else if(SensorVal >= IRSlowThresh){
+    output = SLOWDOWN;
+  }
+
+  else{
+    output = NODETECT;
+  }
+
+  return output;
+
+ }

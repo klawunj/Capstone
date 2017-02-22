977a95402a4ae214ab8eef1c83f5e70b81a0f85c
keskikkw <kate.keskikyla@hotmail.com>
2017-02-22 16:34:54 -0500

    Update Comms.cpp

--- CapstoneArduino/Main/Comms.cpp
+++ CapstoneArduino/Main/Comms.cpp
@@ -25,7 +25,7 @@
   DriveCommands output;
   //Variables
   String InputBuffer;//variable to get the serial value
-  int contest;//store the integer value of the serial value
+  int conIB;//store the integer value of the serial value
   String Speed = "";//store the integer speed value
   String Turn = "";//store the turn value
 
@@ -37,36 +37,36 @@
     InputBuffer = Serial.read();
   }
   
-  contest = InputBuffer.toInt();
-  contest = contest - 48; //convert ascii values to usable numbers
+  conIB = InputBuffer.toInt();
+  conIB = conIB - 48; //convert ascii values to usable numbers
   
   //Check which value was just read and do the correct action based on that value
   
   //If there is a space, switch which variable written to
-  if(contest == -16 && count == 0){
+  if(conIB == -16 && count == 0){
     variable = 1;
     count++;
   }
   
   //if there is a precent sign, new character
-  else if (contest == -11){
+  else if (conIB == -11){
     count = 0;
   }
   
   //If there is #, end of message
-  else if (contest == -13){
+  else if (conIB == -13){
     //Turn strings to integers, set output to integers
     output.Speed() = Speed.toInt();
     output.Turn() = Turn.toInt();
       
     //reset values
-    contest = 0; Speed = ""; Turn = ""; count = 0; variable = 0;
+    conIB = 0; Speed = ""; Turn = ""; count = 0; variable = 0;
     //Return the new Drive Commands
     return output;
   }
   
   //if there is a negative sign, put a negative sign
-  else if (contest == -3 && count == 0){
+  else if (conIB == -3 && count == 0){
     if (variable ==1){Turn = Turn+"-";}
     else{Speed = Speed+"-";}
     count++;
@@ -74,9 +74,9 @@
 
   //If it is none of the characters above, add the number value to the message
   else{
-    if(contest >= 0 && count == 0 ){
-      if (variable ==1){Turn = Turn+contest;}
-      else{Speed = Speed+contest;}
+    if(conIB >= 0 && count == 0 ){
+      if (variable ==1){Turn = Turn+conIB;}
+      else{Speed = Speed+conIB;}
       count++;
     }
   }

55ba3637c160a2647fb2bc05b5c480d9bd6df029
keskikkw <kate.keskikyla@hotmail.com>
2017-02-22 17:58:38 -0500

    Update Comms.cpp

--- CapstoneArduino/Main/Comms.cpp
+++ CapstoneArduino/Main/Comms.cpp
@@ -20,64 +20,80 @@
   }
  }
 
- DriveCommands GetDriveCommands(){
+DriveCommands GetDriveCommands(){
   //output
   DriveCommands output;
   //Variables
-  String InputBuffer;//variable to get the serial value
+  //Variables
+  String inputBuffer;//variable to get the serial value
   int conIB;//store the integer value of the serial value
   String Speed = "";//store the integer speed value
   String Turn = "";//store the turn value
-
+  int intSpeed, intTurn;
+  
   //Flags and checks
   int count = 0;//ensure that each character is read only once
+  bool flag = 0;//ensure that full message is only printed once
   bool variable = 0;//signifies the switch between speed and turn when reading values
-  
-  if (Serial.available()){
-    InputBuffer = Serial.read();
-  }
-  
-  conIB = InputBuffer.toInt();
-  conIB = conIB - 48; //convert ascii values to usable numbers
-  
-  //Check which value was just read and do the correct action based on that value
-  
-  //If there is a space, switch which variable written to
-  if(conIB == -16 && count == 0){
-    variable = 1;
-    count++;
-  }
-  
-  //if there is a precent sign, new character
-  else if (conIB == -11){
-    count = 0;
-  }
-  
-  //If there is #, end of message
-  else if (conIB == -13){
-    //Turn strings to integers, set output to integers
-    output.Speed = Speed.toInt();
-    output.Turn = Turn.toInt();
-      
-    //reset values
-    conIB = 0; Speed = ""; Turn = ""; count = 0; variable = 0;
-    //Return the new Drive Commands
-    return output;
-  }
-  
-  //if there is a negative sign, put a negative sign
-  else if (conIB == -3 && count == 0){
-    if (variable ==1){Turn = Turn+"-";}
-    else{Speed = Speed+"-";}
-    count++;
-  }
 
-  //If it is none of the characters above, add the number value to the message
-  else{
-    if(conIB >= 0 && count == 0 ){
-      if (variable ==1){Turn = Turn+conIB;}
-      else{Speed = Speed+conIB;}
+  while (1){
+    if(Serial.available()){
+    inputBuffer = Serial.read();
+    }
+
+    conIB = inputBuffer.toInt();
+    conIB = conIB - 48; //convert ascii values to usable numbers
+
+    //If there is a space, switch which variable written to
+    if(conIB == -16 && count == 0){
+      variable = 1;
       count++;
     }
+
+    //if there is a precent sign, new character
+    else if (conIB == -11){
+      count = 0;
+    }
+
+    //If there is #, means end of message
+    else if (conIB == -13){
+      //check if the first time sent
+      if (flag==0){
+
+        intSpeed = Speed.toInt();
+        intTurn = Turn.toInt();
+        output.Speed = intSpeed;
+        output.Turn = intTurn;
+
+        //reset values
+        conIB = 0;
+        Speed = "";
+        Turn = "";
+        flag = 1;
+        count = 0;
+        variable = 0;
+
+        //return value
+        return output;
+      }
+    }
+
+    //if there is a negative sign, put a negative sign
+    else if (conIB == -3 && count == 0){
+      if (variable ==1){Turn = Turn+"-";}
+      else{Speed = Speed+"-";}
+      count++;
+      flag = 0;
+    }
+
+    //Add the number value to the message
+    else{
+      if(conIB >= 0 && count == 0 ){
+        if (variable ==1){Turn = Turn+conIB;}
+        else{Speed = Speed+conIB;}
+        count++;
+        flag = 0;
+    }
   }
+ }
 }

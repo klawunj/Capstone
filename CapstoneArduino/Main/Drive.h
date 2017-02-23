ffbdd0eb6444cdec3dc274bfbcceaaacf7af4a01
klawunj <jon_klawunn@hotmail.ca>
2017-02-23 16:33:55 -0500

    Added in changes for Speed/Steering Control

--- CapstoneArduino/Main/Drive.h
+++ CapstoneArduino/Main/Drive.h
@@ -8,23 +8,43 @@
 /*****************************************************************************
  *                                 TypeDefs
  ****************************************************************************/
+ 
+ typedef struct{
+  float looptime;
+  float Ki;
+  float Kp;
+  float Kd;
+  float previousError;
+  float previousIntegral;
+  int StartValue;
+  int OldSetpoint;
+   
+ }PIController;
+
+ typedef enum{
+  SPEED = 0,
+  TURN = 1
+  
+ }ControlMode;
+
 
 /*****************************************************************************
  *                                 Defines
  ****************************************************************************/
 #define motorpin 9
 #define servopin 10
-#define MaxSpeed 90
+#define MaxSpeed 45
 #define MinSpeed 0
-#define MaxRightServoPos 10
-#define MaxLeftServoPos -10
+#define MaxRightServoPos 20
+#define MaxLeftServoPos 0
 /*****************************************************************************
  *                             Function Prototypes
  ****************************************************************************/
  void InitDrive();
+ void InitPIControllers();
  void SetSpeed(int UnMappedSpeed);
  void SetSteering(int UnMappedServoPos);
- void SteerControl(int NewServoPos);
+ int DriveControl(int NewVal, int CurrentVal, ControlMode Mode);
  
 
 

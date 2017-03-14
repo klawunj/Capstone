1b524ebe56ce7179e6cd954d1827eb7dce98a061
NabilHamza_Laptop <nabil.hamza.nah@gmail.com>
2017-03-14 18:09:42 -0400

    Added an intersection finder, now we can use XRIGHT, XLEFT and XmidPoint variables to determine the stering angle

--- Capstone/ImageRec/LaneDetectionTests/laneimages.cpp
+++ Capstone/ImageRec/LaneDetectionTests/laneimages.cpp
@@ -63,10 +63,10 @@ int main(int argc, char* argv[]) {
 	std::cout << image.cols << std::endl; // # Of cols in image
 	std::cout << image.rows << std::endl; // # Of rows in image
 	
-	int top = (image.cols)/4 - 1;
-    int left = (image.rows)/4 - 1;
-    int width = (image.cols)/2  - 1;
-    int height = 3*(image.rows)/4  - 1;
+	int top = 0;
+    int left = 0;
+    int width = image.cols - 1;
+    int height = image.rows - 1;
 	
 	Rect roi(top,left,width,height);// set the ROI for the image
 	Mat imgROI = image(roi);
@@ -80,7 +80,7 @@ int main(int argc, char* argv[]) {
 	imwrite("contour.png", contoursInv);
 	//--------------------Hough Transform-----------------------//
 	std::vector<Vec2f> lines;
-	int houghVote = 100; // HoughLines threshold value (higher ==> less lines, lower ==> more lines)
+	int houghVote = 50; // HoughLines threshold value (higher ==> less lines, lower ==> more lines)
 	
 	/* // Loop for determining proper houghVote
 	if (houghVote < 1 or lines.size() > 2){ // we lost all lines. reset 
@@ -109,7 +109,7 @@ int main(int argc, char* argv[]) {
 		float theta= (*it)[1]; // second element is angle theta
 		
 		// filter to remove vertical and horizontal lines
-		if ( theta > (5*(PI/180)) && theta < (75*(PI/180)) || theta < (175*(PI/180)) && theta > (105*(PI/180)) ) { 
+		if ( theta > (5*(PI/180)) && theta < (85*(PI/180)) || theta < (180*(PI/180)) && theta > (90*(PI/180)) ) { 
 		
 			// point of intersection of the line with first row
 			Point pt1(rho/cos(theta),0);        
@@ -130,8 +130,8 @@ int main(int argc, char* argv[]) {
 	LineFinder ld;
 
     // Set probabilistic Hough parameters
-	ld.setLineLengthAndGap(50,15);
-	ld.setMinVote(6);
+	ld.setLineLengthAndGap(30,20);
+	ld.setMinVote(30);
 	
     // Detect lines
 	
@@ -161,17 +161,91 @@ int main(int argc, char* argv[]) {
 	
 	// Set probabilistic Hough parameters
 	ld.setLineLengthAndGap(5,2);
-	ld.setMinVote(1);
+	ld.setMinVote(10);
 	//Use shifts when drawing on a non ROI image
 	ld.setShiftV(left);
 	ld.setShiftH(top);
 	//std::cout << "Original Bitwise" << "\n";
 	ld.drawDetectedLines(image);
 	
+	int YmidAxis = height/4;
+	int XmidPiont = width/2 + top;
+	
+	// Middle of frame Reference
+	Point Pmid1(XmidPiont, height);        
+	Point Pmid2(XmidPiont,height - 30);
+	line(image, Pmid1, Pmid2, Scalar(0,0,0), 4);
+	
+	// Midpoint Axis
+	Point MidAxis1(0 + top, YmidAxis);        
+	Point MidAxis2(width + top , YmidAxis);
+	line(image, MidAxis1, MidAxis2, Scalar(0,0,255), 4);
+	
+	double XLEFT = 0.0;
+	double XRIGHT = width;
+	
+	// Intersection Finder 
+	for (int i=0; i<li.size(); ++i){
+		double x1 = li[i][0] + top;
+		double y1 = li[i][1] + left;
+		double x2 = li[i][2] + top;
+		double y2 = li[i][3] + left;
+		
+		double YHigh = 0.0;
+		double XHigh = 0.0;
+		double YLow = 0.0;
+		double XLow = 0.0;
+		
+		if ( y2 > y1 ){
+			YHigh = y2;
+			XHigh = x2;
+			YLow = y1;
+			XLow = x1;
+		} else{
+			YHigh = y1;
+			XHigh = x1;
+			YLow = y2;
+			XLow = x2;
+		}
+		
+		// LEFT POINT
+		if ( XHigh < XmidPiont && XLow < XmidPiont){
+			if ( YHigh > YmidAxis && YLow < YmidAxis) {	
+				double slope = (YHigh - YLow)/(XHigh - XLow);
+				double b = YHigh - slope*XHigh;
+				double Ltemp = (YmidAxis - b)/slope;
+				if ( Ltemp > XLEFT){
+					XLEFT = Ltemp;
+				}
+			}
+		}else if ( XHigh > XmidPiont && XLow > XmidPiont) {  //RIGHT POINT
+			if ( YHigh > YmidAxis && YLow < YmidAxis) {	
+				double slope = (YHigh - YLow)/(XHigh - XLow);
+				double b = YHigh - slope*XHigh;
+				double Rtemp = (YmidAxis - b)/slope;
+				if ( Rtemp < XRIGHT){
+					XRIGHT = Rtemp;
+				}
+			}
+		}
+	}
+	
+	// Intersection Lines
+	Point IntL1(XLEFT, 0);        
+	Point IntL2(XLEFT, height);
+	line(image, IntL1, IntL2, Scalar(0,0,255), 4);
+	
+	Point IntR1(XRIGHT, 0);        
+	Point IntR2(XRIGHT, height);
+	line(image, IntR1, IntR2, Scalar(0,0,255), 4);
+	
+	
 	std::cout << "Gathered Bitwise" << "\n";
 	for (int i=0; i<li.size(); ++i){
-		std::cout << "Bitwise Line "<< i << ": [["<< li[i][0] <<"," << li[i][1]+left << "],[" << li[i][2] << "," << li[i][3]+left << "]]\n";
+		std::cout << "Bitwise Line "<< i << ": [["<< li[i][0] + top <<"," << li[i][1] + left << "],[" << li[i][2] + top << "," << li[i][3] + left << "]]\n";
 	}
+	
+	std::cout << "Left X Point: " << XLEFT << " Right X Point: " << XRIGHT << " X Mid Point: " << XmidPiont << std::endl; 
 	
 	/* // Write # of Lines on Image
 	std::stringstream stream;

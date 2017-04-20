996de65bfdfb825fef8f467130a93e1eae05ff29
trent3 <trentstevenson@hotmail.ca>
2017-04-20 13:44:28 -0400

    Update Sensors.cpp
    Added logic for 2 sensors

--- CapstoneArduino/Main/Sensors.cpp
+++ CapstoneArduino/Main/Sensors.cpp
@@ -5,37 +5,55 @@
  /*****************************************************************************
  *                             Global Variables
  ****************************************************************************/
- 
+ int count;
 
   /*****************************************************************************
  *                                 Functions
  ****************************************************************************/
 
- void InitSensors(){
-  pinMode(IRPin, INPUT);
+void InitSensors(){
+  pinMode(IRPinR, INPUT);
+  pinMode(IRPinL, INPUT);
+  count = 0;
  }
 
- int getIRValue(){
-  unsigned int reading = analogRead(IRPin);
-  return reading;
+ SensorVals getIRValue(){
+  SensorVals SV;
+  unsigned int readingR = analogRead(IRPinR);
+  unsigned int readingL = analogRead(IRPinL);
+
+  SV.rSensor = readingR;
+  SV.lSensor = readingL;
+  
+  return SV;
   
  }
 
  SensorBehaviour getSensorBehaviour(){
   SensorBehaviour output;
-  int SensorVal = getIRValue();
+  
+  for(int i = 0; i < 3; i++){
+    SensorVals input = getIRValue();
+    
+    if(input.rSensor >= IRStopThresh || input.lSensor >= IRStopThresh){
+      count++;
+    }
+    
+  }
+    
 
-  if(SensorVal >= IRStopThresh){
+  if(count >= 2){
     output = STOP;
   }
-  else if(SensorVal >= IRSlowThresh){
+  //useless code below since code reads anything other than stop as keep going, change if we implement more speeds
+  else if(count == 1){
     output = SLOWDOWN;
   }
 
   else{
     output = NODETECT;
   }
-
+  count = 0;
   return output;
 
  }
